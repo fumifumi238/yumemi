c4097b0c877064ad7ba0060b9a0b09499c54e696
epthalite <fmy628.1998@icloud.com>
2022-05-03 03:53:48 +0900

    delete apikey

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,13 +4,11 @@ export const fetchApi = async (
 ) => {
   const url = `https://opendata.resas-portal.go.jp` + path;
   const apiKey = process.env.REACT_APP_X_API_KEY;
-  console.log(apiKey);
   const res = await fetch(url, {
     headers: {
       "x-api-key": apiKey ? apiKey : "",
     },
   });
-  console.log(res);
   const data = await res.json();
   fetchData(data);
 };
